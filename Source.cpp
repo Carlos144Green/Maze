b35f53d5c85468439e96f79d7ada725c688c3abc
Carlos Cuartas <60107217+Carlos144Green@users.noreply.github.com>
2022-07-29 16:47:43 -0400

    Set up the auto-line detection on imported mazes

---
+++ Source.cpp
@@ -0,0 +1,432 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <queue>
+#include <stack>
+#include <windows.h>
+#include <chrono>
+#include <thread>
+
+#define CVUI_IMPLEMENTATION
+#include "cvui.h"
+#define WINDOW_NAME "CVUI Test"
+
+HWND hMainWindow;
+
+using namespace cv;
+using namespace std;
+
+int width = (int)GetSystemMetrics(SM_CXSCREEN);
+int height = (int)GetSystemMetrics(SM_CYSCREEN);
+
+
+//i is the rows
+//j is the columns
+class Cell {
+public:
+    int x;
+    int y;
+    vector<bool> walls = { 1,1,1,1 };
+    bool v = false;
+
+    Cell(int x_in, int y_in) {
+        x = x_in;
+        y = y_in;
+    }
+    Cell() {
+        x = NULL;
+        y = NULL;
+    }
+
+    void show(Mat img, int cell_size) {
+        if (v) {
+            cv::rectangle(img, Point(x * cell_size, y * cell_size), Point(x * cell_size + cell_size, y * cell_size + cell_size), Scalar(0, 100, 0), -1);
+        }
+
+        if (walls[0] == 1)
+            cv::line(img, Point(x * cell_size, y * cell_size), Point(x * cell_size + cell_size, y * cell_size), Scalar(255, 255, 255));
+        if (walls[1] == 1)
+            cv::line(img, Point(x * cell_size +cell_size-1, y * cell_size+ cell_size), Point(x * cell_size + cell_size-1, y * cell_size), Scalar(255, 255, 255));
+        if (walls[2] == 1)
+            cv::line(img, Point(x * cell_size, y * cell_size + cell_size-1), Point(x * cell_size + cell_size, y * cell_size+ cell_size-1), Scalar(255, 255, 255));
+        if (walls[3] == 1)
+            cv::line(img, Point(x * cell_size, y * cell_size), Point(x * cell_size , y * cell_size + cell_size), Scalar(255, 255, 255));
+
+
+    }
+};
+
+class Maze {
+public:
+    vector<vector<Cell>> cellMaze;
+    vector<Cell>  row;
+    Point start;
+    Point end;
+    Cell templateCell;
+    Point currCell;
+    Point next;
+    Point mazeSize;
+    stack <Point> stack;
+    vector <Point> touching = { Point(0,-1), Point(1,0), Point(0,1), Point(-1,0) };
+    Point canvasSize;
+    queue <Point> q;
+    bool hasMoves = true;
+
+
+    int cell_size = 0;
+    Maze(Point p, Point mazeStart, Point mazeEnd) { // Constructor with parameters
+        mazeSize = Point(p.x,p.y);
+        row.reserve(mazeSize.y);
+        row.resize(mazeSize.y, templateCell);
+        cellMaze.reserve(mazeSize.x);
+        cellMaze.resize(mazeSize.x, row);
+        start = mazeStart;
+        end = mazeEnd;
+        currCell = end;
+        next = end;
+        q.push(start);
+        populate();
+    }
+
+    void populate() {
+        Cell cell;
+        for (int i = 0; i < mazeSize.x; i++) {
+            for (int j = 0; j < mazeSize.y; j++) {
+                cell = Cell(i, j);
+                cellMaze[i][j] = cell;
+            }
+        }
+    }
+
+ 
+    Point resize() {
+        int x = int(mazeSize.x);
+        int y = int(mazeSize.y);
+        double xScale = width / x;
+        double yScale = height / y;
+
+        if (xScale < yScale)
+            cell_size = xScale * .7;
+        else
+            cell_size = yScale * .7;
+
+        x = int(x * cell_size);
+        y = int(y * cell_size);
+        canvasSize = Point(x+1, y+100);
+        return canvasSize;
+    }
+    void drawFrame(Mat img) {
+
+        for (int i = 0; i < mazeSize.x; i++) {
+            for (int j = 0; j < mazeSize.y; j++) {
+                cellMaze[i][j].show(img, cell_size);
+            }
+        }
+        highlight(img);
+
+    }
+    Point nextMove(Point current) {
+        
+        int i = current.x;
+        int j = current.y;
+        srand((unsigned)time(NULL));
+
+        //cout << i << j << ": ";
+        vector <Point> neighbors;
+
+        for (int k = 0; k < 4; k++) {                       // loops through all neighbors
+            Point thisNeighbor = Point(i, j) + touching[k]; // checks if any neighbors are out of bounds
+            if ((thisNeighbor.x >= 0 && thisNeighbor.x < mazeSize.x) && (thisNeighbor.y >= 0 && thisNeighbor.y < mazeSize.y)) {
+                if (cellMaze[thisNeighbor.x][thisNeighbor.y].v != true) {                  // if neighbor is new
+                    neighbors.push_back(thisNeighbor);   // push to stack?
+                    //cout << thisNeighbor.x << thisNeighbor.y << "  ";
+                }
+            }
+        }
+
+        if (neighbors.size()>0) {
+            return neighbors[rand() % neighbors.size()];
+        }
+        return Point(-1, -1);
+    }
+    void highlight(Mat img) {
+        int x = currCell.x;
+        int y = currCell.y;
+        cv::rectangle(img, Point(x * cell_size + 2, y * cell_size + 2), Point(x * cell_size + cell_size - 2, y * cell_size + cell_size - 2), Scalar(0, 150, 0), -1);
+    }
+
+    void removeWalls(Point a, Point b) {
+        int x = a.x - b.x;
+        int y = a.y - b.y;
+        if (x == 1) {
+            cellMaze[a.x][a.y].walls[3] = false;
+            cellMaze[b.x][b.y].walls[1] = false;
+        }
+        else if (x == -1) {
+            cellMaze[a.x][a.y].walls[1] = false;
+            cellMaze[b.x][b.y].walls[3] = false;
+        }
+        if (y == 1) {
+            cellMaze[a.x][a.y].walls[0] = false;
+            cellMaze[b.x][b.y].walls[2] = false;
+        }
+        else if (y == -1) {
+            cellMaze[a.x][a.y].walls[2] = false;
+            cellMaze[b.x][b.y].walls[0] = false;
+        }
+    }
+
+    bool generateMaze() {
+        if (next != Point(-1, -1)) {
+            removeWalls(next, currCell);
+            currCell = next;
+            cellMaze[currCell.x][currCell.y].v = true;
+
+            stack.push(currCell);
+            next = nextMove(currCell);
+        }
+        else if (stack.size() > 0) {
+            next = stack.top();
+            stack.pop();
+            currCell = next;
+            next = nextMove(currCell);
+        }
+        else {
+            hasMoves = false;
+            for (int i = 0; i < mazeSize.x; i++) {
+                for (int j = 0; j < mazeSize.y; j++) {
+                    cellMaze[i][j].v = false;
+
+                }
+            }
+        }
+        return hasMoves;
+    }
+
+    void BFS(Mat img, int &state) {
+        if (q.size() != 0) {
+            Point node = q.front();
+            q.pop();
+            if (cellMaze[node.x][node.y].v == false) {
+                cellMaze[node.x][node.y].v = true;
+                for (int k = 0; k < 4; k++) {                       // loops through all neighbors
+                    if (cellMaze[node.x][node.y].walls[k] == false) {
+
+                        q.push(touching[k] + node);
+                    }
+                }
+            }
+        }
+        else {
+            state++;
+        }
+    }
+};
+
+void open_file_prompt() {
+    //yash code here
+}
+
+
+void setup() {
+    //srand((unsigned)time(NULL));
+    cvui::init(WINDOW_NAME);
+}
+int drawButtons(Mat img, Maze maze, Point canvasSize) {
+    img = cv::Scalar(0, 0, 0);
+    // More buttons will be added later, I plan on having the buttons gray out after use but that will be a problem for later.
+    if (cvui::button(img, 100, canvasSize.y - 60, "Generate Maze")) {
+        return 1;
+    }
+
+    if (cvui::button(img, 100, canvasSize.y - 30, "Open File")) {
+        return 2;
+    }
+    if (cvui::button(img, 250, canvasSize.y - 60, "Start BFS")) {
+        return 5;
+    }
+    else
+        return 0;
+
+}
+void importMaze() {
+    Mat gray;
+    Mat blur;
+    vector<Vec2f> lines;
+    string file = "C:\\Users\\cuartasca\\Pictures\\maze4.jpg";
+    open_file_prompt();
+    std::string image_path = samples::findFile(file);
+    Mat img = imread(image_path, IMREAD_COLOR);
+    Point imgSize = Point(img.size().width, img.size().height);
+    resize(img, img, Size(550, 550 * img.size().height / img.size().width), 0, 0, INTER_LINEAR);
+    Point resizedSize = Point(img.size().width, img.size().height);
+    Mat imgText = img.clone();
+    string text = "Crop Maze OR Press Enter";
+    Size size = cv::getTextSize(text,cv::FONT_HERSHEY_DUPLEX, 1, 2, 0);
+    int textX = (resizedSize.x - size.width) / 2;
+    int textY = (resizedSize.y + size.height) / 2;
+
+
+    cv::putText(imgText, text, Point(textX, textY), cv::FONT_HERSHEY_DUPLEX, 1, Scalar(30, 150, 0),2);
+    Rect ROIrectangle = cv::selectROI("ROI Selection", imgText, true);
+    //cout << ROIrectangle << endl;
+    Mat ROI;
+    if (ROIrectangle.area() != 0) {
+        ROI = img(ROIrectangle);
+    }
+    else {
+        ROI = img;
+    }
+    cv::destroyWindow("ROI Selection");
+    cv::cvtColor(ROI, gray, cv::COLOR_BGR2GRAY);
+    cv::GaussianBlur(gray, blur, Size(5, 5), 1);
+
+    vector<float> rowSpectrum;
+    cv::Scalar tempVal;
+    for (int i = 0; i < ROI.size().height; i++) {
+        tempVal = cv::mean(blur.row(i));
+        //cout << tempVal.val[0] << endl;
+        rowSpectrum.push_back(float(tempVal.val[0]));
+    }
+
+    Mat out = Mat(rowSpectrum).reshape(1);
+    Scalar mean = cv::mean(rowSpectrum);
+    Point tempPoint;
+    vector<Point> allPassingRows;
+    float last= 255;
+    for (int j = 0; j < ROI.size().height; j++){
+        if (rowSpectrum[j] < mean.val[0]) {
+            if (last > mean.val[0]) {
+                tempPoint.x = j;
+            }
+            if (j == ROI.size().height - 1) {
+                tempPoint.y = j;
+
+                allPassingRows.push_back(tempPoint);
+            }
+        }
+        else {
+            if (last < mean.val[0]) {
+                tempPoint.y = j;
+                allPassingRows.push_back(tempPoint);
+                tempPoint = Point(0, 0);
+            }
+        }
+        last = rowSpectrum[j];
+    }
+    for (int i = 0; i < allPassingRows.size(); i++) {
+        //cout <<"row "<<i<<": "<< allPassingRows[i] << endl;
+        cv::line(ROI, Point(0, int((allPassingRows[i].x + allPassingRows[i].y)/2)), Point(ROI.size().width, int((allPassingRows[i].x + allPassingRows[i].y) / 2)), Scalar(100, 255, 0),1);
+    }
+    vector<Point> allPassingCols;
+    vector<float> colSpectrum;
+    for (int i = 0; i < ROI.size().width; i++) {
+        tempVal = cv::mean(blur.col(i));
+        cout << tempVal.val[0] << endl;
+
+        colSpectrum.push_back(float(tempVal.val[0]));
+    }
+    out = Mat(colSpectrum).reshape(1);
+    mean = cv::mean(colSpectrum);
+    for (int j = 0; j < ROI.size().width; j++) {
+        if (colSpectrum[j] < mean.val[0]) {
+            if (last > mean.val[0]) {
+                tempPoint.x = j;
+            }
+            if (j == ROI.size().width - 1) {
+                tempPoint.y = j;
+                allPassingCols.push_back(tempPoint);
+            }
+        }
+        else {
+            if (last < mean.val[0]) {
+                tempPoint.y = j;
+                allPassingCols.push_back(tempPoint);
+                tempPoint = Point(0, 0);
+            }
+        }
+        last = colSpectrum[j];
+    }
+    for (int i = 0; i < allPassingCols.size(); i++) {
+        //cout << "row " << i << ": " << allPassingCols[i] << endl;
+        cv::line(ROI, Point(int((allPassingCols[i].x + allPassingCols[i].y) / 2),0), Point(int((allPassingCols[i].x + allPassingCols[i].y) / 2), ROI.size().height), Scalar(100, 255, 0), 1);
+    }
+
+    
+
+
+    cvui::imshow("test", img);
+
+
+}
+void stateMachine(Mat img, Maze &maze,int &state, int buttonInput) {
+    if (buttonInput != 0)
+        state = buttonInput;
+    
+
+    switch (state)
+    {
+        case 0:
+            break;
+        case 1:
+            if (maze.generateMaze() == false)
+                state++;
+                break;
+            break;
+        case 2:
+
+            importMaze();
+            state++;
+
+            break;
+        case 3:
+
+            break;        
+        case 4:
+
+            break;
+        case 5:
+            maze.BFS(img, state);
+            break;
+    default:
+        break;
+    }
+
+
+}
+
+int main()
+{
+    setup();
+    Point mazeSize = Point(60, 40);
+    Point mazeStart = Point(0, 0);
+    Point mazeEnd = Point(mazeSize.x - 1, mazeSize.y - 1);
+    Maze maze(mazeSize, mazeStart, mazeEnd);
+
+    Point canvasSize = maze.resize();
+    cv::Mat img = cv::Mat(cv::Size(canvasSize.x, canvasSize.y), CV_8UC3);
+    img = cv::Scalar(0, 0, 0);
+    int buttonInput = 0;
+    int state = 0;
+    while (true) {
+
+        buttonInput = drawButtons(img, maze, canvasSize);
+        stateMachine(img, maze, state, buttonInput);
+        maze.drawFrame(img);
+
+        cvui::imshow(WINDOW_NAME, img);
+        if (cv::waitKey(1) == 27)
+            break;
+    }
+
+
+    return 1;
+}
